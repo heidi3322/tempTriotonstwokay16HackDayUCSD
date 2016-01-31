46024c433915d0b91de57723f065911d732e8ae9
Jonathan Luck <jrluck@hotmail.com>
2016-01-30 17:50:24 -0800

    Movement that works

--- grid.js
+++ grid.js
@@ -1,6 +1,6 @@
 var coordinates = {}
 var numcoords = 0
-var player = {xPos: x, yPos: y};
+var player = {xPos: 640, yPos: 220};
 var charCoords= {}
 
 
@@ -14,45 +14,41 @@ function createHexagon(x, y){
 	
 }
 function canMove(x, y) {
-    if (objValIndexOf(coordinates, [x,y]) == -1) {
-        return false;
-    }
-    return true; 
+	keys = Object.keys(coordinates);
+	inside = false;
+	keys.forEach(function(e){
+		console.log(Math.abs(coordinates[e][0]-x) + " " + Math.abs(coordinates[e][1]-y))
+		if (Math.abs(coordinates[e][0]-x) < 0.75 && Math.abs(coordinates[e][1] - y) < 0.5 ) {
+			inside = true;
+		}
+		
+	})
+    
+    return inside; 
 }
 
 function move_player(x, y) {
-    latitude = x*47.67 
-    longitude = y*27.5
-    if (canMove((player.xPos +latitude), (player.yPos+longitude)) == false) {
+    latitude = round(x*47.67, 2);
+    longitude = round(y*27.5, 2);
+	console.log(latitude + " " + longitude);
+	xPos = round(parseFloat($("#player").css("left").replace("px", "")),2);
+	yPos = round(parseFloat($("#player").css("top").replace("px", "")),2);
+	console.log(xPos + " " + yPos)
+	console.log(round((xPos +latitude),2) + " " + round((yPos + longitude),2));
+    if (canMove(round((xPos +latitude),2), round((yPos+longitude),2)) == false) {
+		console.log(false);
         return;
     }
-    player.xPos += round(x*47.67,2);
-    player.yPos += round(y*27.5, 2); 
+	console.log(true);
+    xPos += round(x*47.67,2);
+    yPos += round(y*27.5, 2);
+	
+	$("#player").css({"top": yPos, "left":xPos});
+	
     
 }
 
-function doKeyDown(e) {
-    var element = document.getElementById("player")
-    alert(e.keyCode)
-    if (e.keyCode == 81) { //q
-        move_player(-1, -1);
-    } 
-    if (e.keyCode == 69) { //e
-        move_player(1, -1); 
-    }
-    if (e.keyCode == 87) { //w
-        move_player(0, -1); 
-    }
-    if (e.keyCode == 83) { //s
-        move_player(0, 1); 
-    }
-    if (e.keyCode == 65) {  //a
-        move_player(-1, 1);
-    } 
-    if (e.keyCode == 68) { //d 
-        move_player(1, 1); 
-    }
-}
+
 function filter(obj){
 	keys = Object.keys(obj);
 	valid = [];
@@ -72,7 +68,7 @@ function add_player(x,y)
 	var img = document.createElement("img");
 	$(img).attr("src","CatSlime.gif");
 	img.src = "CatSlime.gif"+"?a="+Math.random();
-	c = {"position":"absolute", "top":y-10, "left":x};
+	c = {"position":"absolute", "top":y, "left":x};
 	$(img).css(c);
 	charCoords[[x,y]] = [x,y];
 	$(img).attr("id", "player");
@@ -96,7 +92,7 @@ function getNeighbors(x, y){
 function objValIndexOf(obj, search){
 	keys = Object.keys(obj);
 	for(i = 0; i <keys.length;i++){
-		if(obj[keys[i]][0] == search[0] && obj[keys[i]][1] == search[1] ){
+		if(round(obj[keys[i]][0], 1) == round(search[0],1) && round(obj[keys[i]][1],1) == round(search[1],1) ){
 			return i;
 		}
 	}
@@ -114,24 +110,6 @@ function shuffle(o){
     return o;
 }
 
-function randomBranch(x, y, prob){
-	neighbors = filter(getNeighbors(x,y))
-	keys = Object.keys(neighbors);
-	if(keys.length > 0){
-		counter = 0;
-		fillarray = [];
-		emptycounter = 0;
-		emptyarray = [];
-		keys.forEach(function(e){
-			if(Math.random()*100 <= prob){
-				x = neighbors[e][0];
-				y = neighbors[e][1];
-				createHexagon(x,y);
-			}
-		});
-	}
-	
-}
 
 function createRandom(x, y, prob){
 	neighbors = filter(getNeighbors(x,y))
@@ -162,15 +140,28 @@ $(document).ready(function(){
 	x = 640
 	y = 220
 	createHexagon(x, y);
-	add_player(x,y);
 	a = getNeighbors(x,y);
 	createRandom(a[0][0], a[0][1], 100);
-	createRandom(a[1][0], a[1][1], 100);
-	createRandom(a[2][0], a[2][1], 100);
-	createRandom(a[3][0], a[3][1], 100);
-	createRandom(a[4][0], a[4][1], 100);
-	createRandom(a[5][0], a[5][1], 100);
-
+	add_player(640, 220);
 	
-	
+});
+$(document).keydown(function (e) {
+	if (e.keyCode == 81) { //q
+		move_player(-1, -1);
+	} 
+	if (e.keyCode == 69) { //e
+		move_player(1, -1); 
+	}
+	if (e.keyCode == 87) { //w
+		move_player(0, -2); 
+	}
+	if (e.keyCode == 83) { //s
+		move_player(0, 2); 
+	}
+	if (e.keyCode == 65) {  //a
+		move_player(-1, 1);
+	} 
+	if (e.keyCode == 68) { //d 
+		move_player(1, 1); 
+	}
 });
